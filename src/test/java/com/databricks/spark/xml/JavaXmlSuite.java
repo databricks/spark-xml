5889357cac69cae806f00dbfb5aca8db4407e796
hyukjinkwon <gurwls223@gmail.com>
2016-01-08 16:24:58 +0900

    Fix the order of imports

--- src/test/java/com/databricks/spark/xml/JavaXmlSuite.java
+++ src/test/java/com/databricks/spark/xml/JavaXmlSuite.java
@@ -18,7 +18,6 @@ package com.databricks.spark.xml;
 import java.io.File;
 import java.util.HashMap;
 
-import com.databricks.spark.xml.util.XmlFile;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -26,6 +25,7 @@ import org.junit.Test;
 
 import org.apache.spark.SparkContext;
 import org.apache.spark.sql.*;
+import com.databricks.spark.xml.util.XmlFile;
 
 public class JavaXmlSuite {
     private transient SQLContext sqlContext;
